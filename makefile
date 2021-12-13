1fc89a01aec940fab30f3ee834c683bedcd85d23
vanshv <vanshvardhan.singh@iiitb.ac.in>
2021-12-13 10:25:04 +0530

    sriram optimised some code

--- makefile
+++ makefile
@@ -1,2 +1,16 @@
-swoosh-swoosh: main.cpp
-	g++ -o swoosh-swoosh main.cpp
+
+
+CXXFLGS = -I ./inc
+VPATH = ./src
+
+obj = main.o
+exec = swoosh-swoosh 
+
+$(exec): $(obj)
+	$(CXX) $(obj) -o $@
+
+$(obj): %.o: %.cpp
+	$(CXX) -c $(CXXFLAGS) $(CPPFLAGS) $< -o $@
+
+clean:
+	rm -f $(obj) $(exec)
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
-#include<stdio.h>
+#include<iostream>
 #include<unistd.h>
 
 int main(){
     bool flip = 1;
+
+    bool exitGame = 0;
+
     while(1){
+
+        if(exitGame)
+            return 0;
+        
+
+        for(int i = 0; i<14; i++){
+            printf("\n               ");
+        }
         if(flip){
         printf(" ROFL:ROFL:        \n                        _^___\n                     __/   [] \\ \n               LOL===__        \\ \n                       \\________]\n                        I   I\n                       --------/\n               ");
         flip = 0;
@@ -12,10 +23,11 @@ int main(){
         printf("          :ROFL:ROFL\n                        _^___\n                L    __/   [] \\ \n                O ===__        \\ \n                L      \\________]\n                        I   I\n                       --------/\n               ");
         flip = 1;
         }
-        for(int i = 0; i<20; i++){
-            printf("\n               ");
-        }
+
         usleep(300000);
+        
+        system("clear");        
+
     }
 
     return 0;
